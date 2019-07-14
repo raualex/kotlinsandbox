680fb34fbd031e2b54f4d731e9317d3d93b1cfbe
Alexander Rau <alexanderrau86@yahoo.com>
2019-07-14 11:47:48 -0600

    Kotlin chapter 2

---
+++ src/TypeIntro.kt
@@ -0,0 +1,14 @@
+const val MAX_EXPERIENCE = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    var hasSteed = ""
+    var pubName = "Unicorn's Horn"
+    var playerGold = 50
+    var pulicanName = "Angus"
+    var availablePubDrinks = ""
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+}

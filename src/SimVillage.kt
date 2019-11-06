432d429bd6f2ee3fcf4502856e442ece088fccf1
Alexander Rau <alexanderrau86@yahoo.com>
2019-11-06 16:01:33 -0700

    Finish Chapter 5 exercises

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,12 +1,33 @@
 fun main(args: Array<String>) {
-    runSimulation("Guyal") { playerName, numBuildings ->
+    runSimulation()
+}
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+    println(greetingFunction("Guyal"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return { playerName: String ->
         val currentYear = 2018
-        println("Adding $numBuildings houses")
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 }
 
-inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last()  //Randomly selects 1, 2, or 3
-    println(greetingFunction(playerName, numBuildings))
-}
+//inline fun runSimulation(playerName: String,
+//                         costPrinter: (Int) -> Unit,
+//                         greetingFunction: (String, Int) -> String) {
+//    val numBuildings = (1..3).shuffled().last()  //Randomly selects 1, 2, or 3
+//    costPrinter(numBuildings)
+//    println(greetingFunction(playerName, numBuildings))
+//}
+//
+//fun printConstructionCost(numBuildings: Int) {
+//    val cost = 500
+//    println("construction cost: ${cost * numBuildings}")
+//}

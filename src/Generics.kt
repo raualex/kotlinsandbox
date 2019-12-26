2963c2d3cb380bf8248e9d187d11d8f400796b84
Alexander Rau <alexanderrau86@yahoo.com>
2019-12-26 16:32:49 -0700

    Finish Chapter 17

--- src/Generics.kt
+++ src/Generics.kt
@@ -1,13 +1,15 @@
-class LootBox<T : Loot>(item: T) {
+class LootBox<T : Loot>(vararg item: T) {
     var open = false
-    private var loot: T = item
+    private var loot: Array<out T> = item
 
-    fun fetch(): T? {
-        return loot.takeIf { open }
+    operator fun get(index: Int): T? = loot[index].takeIf { open }
+
+    fun fetch(item: Int): T? {
+        return loot[item].takeIf { open }
     }
 
-    fun <R> fetch(lootModFunction: (T) -> R): R? {
-        return lootModFunction(loot).takeIf { open }
+    fun <R> fetch(item: Int, lootModFunction: (T) -> R): R? {
+        return lootModFunction(loot[item]).takeIf { open }
     }
 }
 
@@ -18,16 +20,20 @@ class Fedora(val name: String, value: Int) : Loot(value)
 class Coin(value: Int) : Loot(value)
 
 fun main(args: Array<String>) {
-    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15),
+                                              Fedora("a dazzling magenta fedora", 25))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
 
     lootBoxOne.open = true
-    lootBoxOne.fetch()?.run {
+    lootBoxOne.fetch(1)?.run {
         println("You retrieve $name from the box!")
     }
 
-    val coin = lootBoxOne.fetch() {
+    val coin = lootBoxOne.fetch(0) {
         Coin(it.value * 3)
     }
     coin?.let { println(it.value) }
+
+    val fedora = lootBoxOne[1]
+    fedora?.let { println(it.name) }
 }
---
+++ src/Variance.kt
@@ -0,0 +1,11 @@
+class Barrel<in T>(item: T)
+//class Barrel<out T>(val item: T)
+
+fun main(args: Array<String>) {
+    var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+
+//    lootBarrel = fedoraBarrel
+    fedoraBarrel = lootBarrel
+//    val myFedora: Fedora = lootBarrel.item
+}
